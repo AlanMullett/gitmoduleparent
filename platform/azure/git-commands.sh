0fef6e0bb0caa205b3514247723f7d8add8c08a0
Alan Mullett <alan.mullett@systemidle.com>
2022-11-24 16:40:49 +0000

    update commit for submodule

--- platform/azure/git-commands.sh
+++ platform/azure/git-commands.sh
@@ -1,6 +1,9 @@
 git tag -a tlevels-resac-v1 -m "T Levels Results and Certification Pipeline"
 
 git submodule add https://github.com/SkillsFundingAgency/funding-platform-building-blocks.git
-git config -f .gitmodules submodule.funding-platform-building-blocks.branch resourceTags
+#git config -f .gitmodules submodule.funding-platform-building-blocks.branch resourceTags
 
 git submodule update --remote
+
+git clone --recurse-submodules https://github.com/AlanMullett/gitmoduleparent.git 
+
